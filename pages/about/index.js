a3a9e19dc4f86a8814a65178e7f10657ecda8f67
Amir Azimi <azymy3354@gmail.com>
2024-08-09 16:37:37 +0330

    add: about-page template

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import PageHeader from "@/components/modules/PageHeader/PageHeader";
+import Story from "@/components/templates/About/Story";
 
 function About() {
-  return <h1>About Page :))</h1>;
+  return (
+    <>
+      <PageHeader route="About" />
+      <Story />
+    </>
+  )
 }
 
 export default About;
